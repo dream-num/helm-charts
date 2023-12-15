a7cf99b21fcba7409bfa55807742acb61e372369
zsq1234 <2387311298@qq.com>
2023-12-15 20:15:42 +0800

    feat: add mac sed and change readme

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+PLATFORM=$(uname)
+SED="sed -i"
+if [ "$PLATFORM" == "Darwin" ]; then
+    SED="sed -i \"\""
+fi
+
 cp ./configs/config.yaml.template ./configs/config.yaml
 
 while IFS='=' read -r name value ; do
     # Replace variable with value. 
-    sed -i 's|${'"${name}"'}|'"${value}"'|' ./configs/config.yaml
+    $SED -e 's|${'"${name}"'}|'"${value}"'|' ./configs/config.yaml
 done < .env
 
 docker compose down
