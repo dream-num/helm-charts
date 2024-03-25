cf40af663edfe5bdf509b141a7acb13f50e77b8d
zsq1234 <2387311298@qq.com>
2024-03-25 15:39:46 +0800

    feat: add s3 config for universer

--- docker-compose/run.dev.sh
+++ docker-compose/run.dev.sh
@@ -6,7 +6,7 @@ while IFS='=' read -r name value ; do
     # Replace variable with value. 
     sed -i 's|${'"${name}"'}|'"${value}"'|' ./configs/config.yaml
 done < <(envsubst '$POSTGRES_PASSWORD $RABBITMQ_PASSWORD $CLICKHOUSE_PASSWORD $UNIVERSER_ADMIN_PASSWORD $GRAFANA_PASSWORD \
-    $S3_USER $S3_PASSWORD $S3_REGION $S3_PATH_STYLE $S3_ENDPOINT $S3_ENDPOINT_PUBLIC'< .env.dev)
+    $S3_USER $S3_PASSWORD $S3_REGION $S3_PATH_STYLE $S3_ENDPOINT $S3_ENDPOINT_PUBLIC $S3_DEFAULT_BUCKET'< .env.dev)
 
 docker compose -f docker-compose.dev.yaml down
 
