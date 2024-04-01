2a0a4aba5b9d8fec397dbb71c3989023620a9965
zsq1234 <41423998+zsq1234@users.noreply.github.com>
2024-04-01 11:44:57 +0800

    feat: add mysql driver for docker compose (#67)

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -15,13 +15,26 @@ if [! $DOCKER_COMPOSE version] &> /dev/null; then
     exit 1
 fi
 
+. .env
+
+DATABASE_DSN='host=${DATABASE_HOST} port=${DATABASE_PORT} dbname=${DATABASE_DBNAME} user=${DATABASE_USERNAME} password=${DATABASE_PASSWORD} sslmode=disable TimeZone=Asia/Shanghai'
+if [ "$DATABASE_DRIVER" == "mysql" ]; then
+    DATABASE_DSN='${DATABASE_USERNAME}:${DATABASE_PASSWORD}@tcp(${DATABASE_HOST}:${DATABASE_PORT})/${DATABASE_DBNAME}?charset=utf8mb4\&parseTime=True\&loc=Local'
+fi
+
+COMPOSE_FILE="docker-compose.yaml"
+if [ "$DATABASE_DRIVER" == "mysql" ]; then
+    COMPOSE_FILE="docker-compose.mysql.yaml"
+fi
+
 cp ./configs/config.yaml.template ./configs/config.yaml
+$SED -e 's|${DATABASE_DSN}|'"${DATABASE_DSN}"'|' ./configs/config.yaml
 
 while IFS='=' read -r name value ; do
     # Replace variable with value. 
     $SED -e 's|${'"${name}"'}|'"${value}"'|' ./configs/config.yaml
 done < .env
 
-$DOCKER_COMPOSE down
+$DOCKER_COMPOSE -f $COMPOSE_FILE down
 
-$DOCKER_COMPOSE up -d
+$DOCKER_COMPOSE -f $COMPOSE_FILE up -d
