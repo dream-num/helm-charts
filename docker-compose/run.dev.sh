cf1bd99c42374c874e8dc8d22f2dc96cf807a5a0
zsq1234 <2387311298@qq.com>
2024-04-01 11:44:18 +0800

    feat: add mysql driver for docker compose

--- docker-compose/run.dev.sh
+++ docker-compose/run.dev.sh
@@ -5,7 +5,7 @@ cp ./configs/config.yaml.template ./configs/config.yaml
 while IFS='=' read -r name value ; do
     # Replace variable with value. 
     sed -i 's|${'"${name}"'}|'"${value}"'|' ./configs/config.yaml
-done < <(envsubst '$POSTGRES_PASSWORD $RABBITMQ_PASSWORD $CLICKHOUSE_PASSWORD $UNIVERSER_ADMIN_PASSWORD $GRAFANA_PASSWORD \
+done < <(envsubst '$DATABASE_PASSWORD $RABBITMQ_PASSWORD $CLICKHOUSE_PASSWORD $UNIVERSER_ADMIN_PASSWORD $GRAFANA_PASSWORD \
     $S3_USER $S3_PASSWORD $S3_REGION $S3_PATH_STYLE $S3_ENDPOINT $S3_ENDPOINT_PUBLIC $S3_DEFAULT_BUCKET'< .env.dev)
 
 docker compose -f docker-compose.dev.yaml down
