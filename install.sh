66d4966c3f37f3ebf374ce8c7fc3bdda04782a14
zsq1234 <2387311298@qq.com>
2024-06-15 17:46:00 +0800

    feat: update install script

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ mkdir -p docker-compose \
     && wget https://release-univer.oss-cn-shenzhen.aliyuncs.com/release-demo/docker-compose.zip -O univer.zip \
     && unzip univer.zip \
     && rm univer.zip \
+    && if [ -f ../license.zip ]; then cp ../license.zip . && unzip license.zip -d configs && rm license.zip; fi \
     && bash run.sh
 
 # check universer start by 8000 port in loop
@@ -42,5 +43,7 @@ for i in {1..100}; do
     fi
     sleep 1
 done
+
+sleep 5
 
 docker run --net=univer-prod --rm --name univer-collaboration-lite -p 3010:3010 univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/univer-collaboration-lite:latest
