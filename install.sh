d96a77b25bbdf6b28413374cd95b8eac944cde9f
zsq1234 <2387311298@qq.com>
2024-08-30 20:19:08 +0800

    fix: bash -c BASH_SOURCE[0] problem

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ else
     exit 1
 fi
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(dirname $(realpath "$0"))"
 
 # check curl command
 if ! [ -x "$(command -v curl)" ]; then
