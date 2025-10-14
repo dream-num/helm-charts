e2e48c8cfa2b1fd3956005acf2c6690b0b099476
白熱 <sonne@asaki.me>
2025-10-14 15:58:34 +0800

    fix: bitnami docker image not available (#230)
    * chore: update repository
    
    * fix: bitnami docker image not available
    
    ---------
    
    Co-authored-by: GitHub Actions <zsq1234@users.noreply.github.com>
    Co-authored-by: zsq1234 <2387311298@qq.com>

--- .github/all-in-one/docker-compose/build.sh
+++ .github/all-in-one/docker-compose/build.sh
@@ -23,9 +23,9 @@ pull_image nginx:alpine-slim
 pull_image univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/univer-collaboration:${COLLABORATION_SERVER_VERSION}
 pull_image postgres:${POSTGRES_VERSION}
 pull_image rabbitmq:${RABBITMQ_VERSION}
-pull_image bitnami/redis:${REDIS_VERSION}
+pull_image univerai/redis:${REDIS_VERSION}
 pull_image temporalio/auto-setup:${TEMPORAL_VERSION}
-pull_image bitnami/minio:${MINIO_VERSION}
+pull_image univerai/minio:${MINIO_VERSION}
 pull_image univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/worker-exchange:${UNIVER_WORKER_EXCHANGE_VERSION}
 pull_image univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/univer-collaboration-lite:${UNIVER_DEMO_UI_VERSION}
 pull_image univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/universer-check:0.0.1
@@ -38,9 +38,9 @@ docker save \
     univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/univer-collaboration:${COLLABORATION_SERVER_VERSION} \
     postgres:${POSTGRES_VERSION} \
     rabbitmq:${RABBITMQ_VERSION} \
-    bitnami/redis:${REDIS_VERSION} \
+    univerai/redis:${REDIS_VERSION} \
     temporalio/auto-setup:${TEMPORAL_VERSION} \
-    bitnami/minio:${MINIO_VERSION} \
+    univerai/minio:${MINIO_VERSION} \
     univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/worker-exchange:${UNIVER_WORKER_EXCHANGE_VERSION} \
     univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/univer-collaboration-lite:${UNIVER_DEMO_UI_VERSION} \
     univer-acr-registry.cn-shenzhen.cr.aliyuncs.com/release/universer-check:0.0.1 \
@@ -52,17 +52,17 @@ echo "save observability image"
 pull_image grafana/loki:${LOKI_VERSION}
 pull_image grafana/promtail:${PROMTAIL_VERSION}
 pull_image grafana/grafana:${GRAFANA_VERSION}
-pull_image bitnami/prometheus:${PROMETHEUS_VERSION}
+pull_image univerai/prometheus:${PROMETHEUS_VERSION}
 pull_image oliver006/redis_exporter:${REDIS_EXPORTER_VERSION}
-pull_image bitnami/postgres-exporter:${POSTGRES_EXPORTER_VERSION}
+pull_image univerai/postgres-exporter:${POSTGRES_EXPORTER_VERSION}
 pull_image kbudde/rabbitmq-exporter:${RABBITMQ_EXPORTER_VERSION}
 docker save \
     grafana/loki:${LOKI_VERSION} \
     grafana/promtail:${PROMTAIL_VERSION} \
     grafana/grafana:${GRAFANA_VERSION} \
-    bitnami/prometheus:${PROMETHEUS_VERSION} \
+    univerai/prometheus:${PROMETHEUS_VERSION} \
     oliver006/redis_exporter:${REDIS_EXPORTER_VERSION} \
-    bitnami/postgres-exporter:${POSTGRES_EXPORTER_VERSION} \
+    univerai/postgres-exporter:${POSTGRES_EXPORTER_VERSION} \
     kbudde/rabbitmq-exporter:${RABBITMQ_EXPORTER_VERSION} \
     | gzip > observability-image.tar.gz
 
